a2bf89af3214798fad592c4ff23f919cb0e48691
Breona <breona@eloquentdigitalmarketing.net>
2024-12-10 15:52:52 -0500

    Initial Commit

---
+++ GitHub/GitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubApp.swift
+//  GitHub
+//
+//  Created by Breona Clawson on 12/10/24.
+//
+
+import SwiftUI
+
+@main
+struct GitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
